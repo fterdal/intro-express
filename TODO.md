3fd63e5e3b3f0c2e08e1f6352871c651f77cdf20
Finn <finnterdal@gmail.com>
2020-03-09 14:07:56 -0500

    finished demo part 1

--- TODO.md
+++ TODO.md
@@ -1,4 +1,10 @@
+# Express Code Demo
+
+## Express 101
 * package scripts
+* GET route
+
+## Express 201
 * logging, morgan
 * URL params
 * app.use and next
--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res, next) => {
-  res.send("<h1>Hello! Welcome to the homepage!</h1>")
+  res.send(`
+    <div>
+      <a href="/sandwiches">Go To Sandwiches</a>
+      <h1>Hello! Welcome to the homepage!</h1>
+    </div>
+  `)
 })
 
+app.get("/sandwiches", (req, res, next) => {
+  res.send("<h1>Have some tasty sandwiches! 🍔 🥪</h1>")
+})
 
 app.listen(8080, () => {
   console.log("Listening on port 8080")
