542aa805dd3b9a489d0f6e910fe14c6909569404
Finn <finnterdal@gmail.com>
2020-03-09 15:26:42 -0500

    finish lecture 2

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express")
-// const morgan = require("morgan")
-// const path = require("path")
+const morgan = require("morgan")
+const path = require("path")
 
 const app = express()
+
+// app.use((req, res, next) => {
+//   console.log(req.method, req.path)
+//   next()
+// })
+
+app.use(morgan("dev"))
+
+console.log("\n\n")
+console.log(__dirname)
+console.log("\n\n")
+
+app.use(express.static(path.join(__dirname, "images")))
+// kitty1.png
+// app.get("/kitty1.png", (req, res, next) => {
+//   console.log("Retrieving a cat!");
+//   res.sendFile("/images/kitty1.png");
+// });
 
 app.get("/", (req, res, next) => {
   res.send(`
@@ -15,6 +33,21 @@ app.get("/", (req, res, next) => {
 
 app.get("/sandwiches", (req, res, next) => {
   res.send("<h1>Have some tasty sandwiches! 🍔 🥪</h1>")
+})
+
+app.get("/kitties", (req, res, next) => {
+  res.send(`
+  <div>
+    <img src="/kitty1.png">
+    <img src="/kitty2.png">
+    <img src="/kitty3.png">
+  </div>
+  `)
+})
+
+app.get("/kitties/:kittyId", (req, res, next) => {
+  console.log(req.params)
+  res.send(`<img src="/kitty${req.params.kittyId}.png">`);
 })
 
 app.listen(8080, () => {
